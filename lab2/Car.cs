6167a7d4547a4c278c3fdcbfe0c87a50012483a8
Di <b.dima1916@gmail.com>
2023-03-11 20:18:33 +0200

    add a new taxi class and some features

--- lab2/Car.cs
+++ lab2/Car.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Reflection;
 using System.Security.Cryptography.X509Certificates;
@@ -11,10 +12,12 @@ namespace lab2
 {
     public class Car: IComparable
     {
-        private string model { get; set; }
-        private double initialCost { get; set; }
-        private uint serviceYear { get; set; }
-
+        private string model;
+        private double initialCost;
+        private uint serviceYear; 
+        public string Model { get { return model; } set { model = value; } }
+        public double InitialCost { get { return initialCost; } set { initialCost = value; } }
+        public uint ServiceYear { get { return serviceYear; } set { serviceYear = value; } }
         public Car() 
         {
             model = string.Empty; initialCost = 0.0; serviceYear = 0; 
@@ -25,9 +28,15 @@ namespace lab2
             this.initialCost = _initialCost;
             this.serviceYear = _serviceYear;
         }
+        public Car(Car c)
+        {
+            model = c.model;
+            initialCost = c.initialCost;
+            serviceYear = c.serviceYear;
+        }
         public override string ToString()
         {
-            return $"Car model {model},cost {appraisedCost}-usd,{serviceYear} year(s)";
+            return $"Car model {model},{serviceYear} year(s),cost {appraisedCost}-usd";
         }
         public double appraisedCost 
         {
@@ -72,5 +81,38 @@ namespace lab2
         {
             return !(first == other); 
         }
+    }
+    public class Taxi: Car
+    {
+        private string firm;
+        private uint k;
+        public Taxi(): base()
+        {
+            firm= string.Empty; k = 1;
+        }
+        public Taxi(string _model, double _initialCost, uint _serviceYear, string _firm,uint _k):base(_model, _initialCost, _serviceYear)
+        {
+            firm = _firm; k = _k;
+        }
+        public Taxi(Car c,string _firm,uint _k):base(c)
+        {
+            firm = _firm;
+            k = _k;
+        }  
+        public override string ToString()
+        {
+            return base.ToString() + $" ,taxi appraised cost-{appraisedCost},taxi firma - {firm}";
+        }
+        public new double appraisedCost
+        {
+            get
+            {
+                double temp = base.InitialCost;
+                for (int i = 0; i < base.ServiceYear; i++)
+                    temp -= (temp * k * 0.1);
+                return temp;
+            }
+        }
+
     }
 }
--- lab2/Program.cs
+++ lab2/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,12 +11,33 @@ namespace lab2
     {
         static void Main(string[] args)
         {
-            Car c = new Car("bmw",2000,5);
+            Car c = new Car("bmw",200000,5);
             Car w = new Car("mercedes", 2000, 4);
+            Taxi t = new Taxi("bmw", 2000, 5, "dou" , 2);
+            Taxi d = new Taxi("bmw", 150, 5, "dou", 2);
+            Taxi x = new Taxi(w, "dou", 3);
+            Car[] arrCar = new Car[5] { c, w, t, d, x };
             Console.WriteLine(c);
             Console.WriteLine(w);
             bool triger = c==w;
             Console.WriteLine(triger);
+            Console.WriteLine(d);
+            Console.WriteLine(t);
+            bool trig = t > d;
+            Console.WriteLine(trig);
+            Console.WriteLine(cheapestCar(arrCar));
+        }
+        public static Car cheapestCar(Car[] arrCar) 
+        {
+            Car min = arrCar[0];
+            for(int i =1 ; i < arrCar.Length; i++)
+            {
+                if (min > arrCar[i])
+                {
+                    min = arrCar[i];
+                }
+            }
+            return min;
         }
     }
 }
